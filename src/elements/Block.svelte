ac2e84fc792e6c7d66ae0b5e706d1dbe719dd9fe
MohamedElmdary <engm5081@gmail.com>
2022-10-29 16:00:23 +0200

    Add block element

---
+++ src/elements/Block.svelte
@@ -0,0 +1,14 @@
+<svelte:options tag="b-block" />
+
+<script context="module" lang="ts">
+  import { get_current_component } from "svelte/internal"
+  import { applyHostClass } from "../internals"
+</script>
+
+<script lang="ts">
+  const host = get_current_component()
+
+  applyHostClass(host, { block: true })
+</script>
+
+<slot />
