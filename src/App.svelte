f4a1554dc9ddfc2f3960bf3234e5ada85787acfa
MohamedElmdary <engm5081@gmail.com>
2022-10-30 15:08:30 +0200

    Add Input element

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,28 @@
 <svelte:options tag="b-app" />
 
 <script lang="ts">
-  import Table from "./elements/Table.svelte"
-  let headers = ["Name", "Weight", "Certified"]
+  import { fb, validators, form } from "rx-svelte-forms"
+  import Input from "./elements/Input.svelte"
+
+  const userForm = fb.group({
+    email: [null as number, [validators.required(), validators.min(5)]],
+  })
+
+  const email = userForm.get("email")
 </script>
 
-<Table
+<form use:form={userForm}>
+  <!-- prettier-ignore -->
+  <Input
+    label="Email Address"
+    placeholder="User's email address"
+    controller={email}
+    type="number"
+  />
+  <!-- <b-input placeholder="User's email address"> Email Address </b-input> -->
+</form>
+
+<!-- <Table
   {headers}
   rows={[
     ["Mohamed", 19, true],
@@ -31,4 +48,4 @@
       },
     },
   ]}
-/>
+/> -->
---
+++ src/elements/Input.svelte
@@ -0,0 +1,53 @@
+<svelte:options tag="b-input" />
+
+<script context="module" lang="ts">
+  import { onMount } from "svelte/internal"
+  import { assertIn, initElement } from "../internals"
+  import { form, type FormControl } from "rx-svelte-forms"
+</script>
+
+<script lang="ts">
+  let host: Element
+
+  export let type: "text" | "email" | "password" | "number" = "text"
+  $: assertIn(type, ["text", "email", "password", "number"], "type")
+
+  export let label: string = ""
+  export let placeholder: string = undefined
+  export let controller: FormControl<string | number> = undefined
+  let value: string | number
+
+  onMount(() => {
+    value = controller.value
+    initElement(host.parentNode as Element)()
+  })
+
+  $: ctrl = $controller
+  $: valid = ctrl ? ctrl.valid && (ctrl.dirty || ctrl.touched) : false
+  $: invalid = ctrl ? ctrl.invalid && (ctrl.dirty || ctrl.touched) : false
+</script>
+
+{#if ctrl}
+  {JSON.stringify(ctrl)}
+  <div class="field" bind:this={host}>
+    <span class="label">
+      {label}
+    </span>
+    <div class="control">
+      <input
+        class="input"
+        class:is-success={valid}
+        class:is-danger={invalid}
+        {type}
+        {placeholder}
+        {value}
+        use:form={controller}
+      />
+    </div>
+    {#if invalid}
+      <p class="help is-danger">
+        {Object.keys(ctrl.errors)}
+      </p>
+    {/if}
+  </div>
+{/if}
