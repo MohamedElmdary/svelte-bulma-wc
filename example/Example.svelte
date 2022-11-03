630db7dd9dd58773bd064c8a2d6d7e01a26843f0
MohamedElmdary <engm5081@gmail.com>
2022-11-03 18:59:24 +0200

    Add leading/trailing icon

---
+++ example/Example.svelte
@@ -0,0 +1,20 @@
+<svelte:options tag="tf-example" />
+
+<script context="module" lang="ts">
+  import * as Bulma from "../src/public_api"
+  import { FormControl, validators } from "rx-svelte-forms"
+</script>
+
+<script lang="ts">
+  const name = new FormControl("", [
+    validators.required("Name is required."),
+    validators.minLength("Name minLength is 3 chars.", 3),
+    validators.maxLength("Name maxLength is 12 chars.", 12),
+  ])
+</script>
+
+<Bulma.Input
+  label="Cluster Name"
+  placeholder="Your kubernetes cluster name"
+  controller={name}
+/>
--- src/elements/Input.svelte
+++ src/elements/Input.svelte
@@ -1,7 +1,7 @@
 <svelte:options tag="b-input" />
 
 <script context="module" lang="ts">
-  import { onMount } from "svelte/internal"
+  import { onMount } from "svelte"
   import { assertIn, initElement } from "../internals"
   import { form, type FormControl } from "rx-svelte-forms"
 </script>
@@ -25,9 +25,11 @@
     initElement(host.parentNode as Element)()
   })
 
+  $: usedType = type
   $: ctrl = $controller
-  $: valid = ctrl ? ctrl.valid : false
-  $: invalid = ctrl ? ctrl.invalid && (ctrl.dirty || ctrl.touched) : false
+  $: valid = validation && (ctrl ? ctrl.valid : false)
+  $: invalid =
+    validation && (ctrl ? ctrl.invalid && (ctrl.dirty || ctrl.touched) : false)
 </script>
 
 <div class="field" bind:this={host}>
@@ -35,12 +37,16 @@
     <span class="label">
       {label}
     </span>
-    <div class="control" class:is-loading={loading}>
+    <div
+      class="control"
+      class:is-loading={loading}
+      class:has-icons-right={valid && type !== "password"}
+    >
       {#if type === "textarea"}
         <textarea
           class="textarea"
-          class:is-success={validation && valid}
-          class:is-danger={validation && invalid}
+          class:is-success={valid}
+          class:is-danger={invalid}
           {placeholder}
           use:form={controller}
           value={ctrl.value}
@@ -51,19 +57,45 @@
           class="input"
           class:is-success={valid}
           class:is-danger={invalid}
-          {type}
+          type={usedType}
           {placeholder}
           use:form={controller}
           value={ctrl.value}
           {disabled}
         />
+
+        {#if type === "password"}
+          <!-- fas far fa-eye-slash -->
+          <!-- fas fa-eye -->
+          <b-icon
+            class="icon"
+            style:position="absolute"
+            style:top="50%"
+            style:right="10px"
+            style:transform="translateY(-50%)"
+            style:z-index="999"
+            style:cursor="pointer"
+            size="small"
+            icon={usedType === "password"
+              ? "fas fa-eye fa-eye-slash"
+              : "fas fa-eye"}
+            on:mousedown={() =>
+              (usedType = usedType === "password" ? "text" : "password")}
+          />
+        {:else if valid}
+          <b-icon
+            class="icon is-right"
+            color="success"
+            icon="fas fa-check fa-sm"
+          />
+        {/if}
       {/if}
     </div>
     {#if hint}
-      <p class="help" class:is-success={validation && valid}>
+      <p class="help" class:is-success={valid}>
         {hint}
       </p>
-    {:else if validation && invalid && ctrl.error}
+    {:else if invalid && ctrl.error}
       <p class="help is-danger">
         {ctrl.error}
       </p>
